c9c628511db52e5347aa4788ab900efe853147f3
Roge <santiago.celaya.rogelio@gmail.com>
2023-06-12 07:08:33 -0600

    Modificacion de metodo con pares

--- src/Rango.java
+++ src/Rango.java
@@ -7,25 +7,27 @@
  *
  * @author Roge
  */
-
 import java.util.Scanner;
 
 /**
  * Clase que imprime los números entre dos valores ingresados por el usuario.
  */
 public class Rango {
+
     /**
-     * Método principal que solicita dos números al usuario y llama al método imprimirNumerosEnRango
-     * para imprimir los números en el rango.
-     * @param args Los argumentos de la línea de comandos (no se utilizan en este programa).
+     * Método principal que solicita dos números al usuario y llama al método
+     * imprimirNumerosEnRango para imprimir los números en el rango.
+     *
+     * @param args Los argumentos de la línea de comandos (no se utilizan en
+     * este programa).
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ingrese el primer número: ");
+        System.out.print("Ingrese el primer numero: ");
         int numero1 = scanner.nextInt();
 
-        System.out.print("Ingrese el segundo número: ");
+        System.out.print("Ingrese el segundo numero: ");
         int numero2 = scanner.nextInt();
 
         scanner.close();
@@ -34,7 +36,9 @@ public class Rango {
     }
 
     /**
-     * Imprime los números en el rango especificado (sin incluir los números de entrada).
+     * Imprime los números en el rango especificado (sin incluir los números de
+     * entrada).
+     *
      * @param inicio El primer número del rango (excluido).
      * @param fin El segundo número del rango (excluido).
      */
@@ -44,8 +48,13 @@ public class Rango {
             return;
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = inicio + 1; i < fin; i++) {
-            System.out.println(i);
+            if (i % 2 == 0) {
+                sb.append(i).append("\n");
+            }
         }
+        System.out.print(sb.toString());
     }
+
 }
--- test/RangoTest.java
+++ test/RangoTest.java
@@ -3,9 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
  */
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 /**
  *
  * @author Roge
@@ -14,19 +11,25 @@ import static org.junit.Assert.*;
 /**
  * Clase de prueba para la clase Rango.
  */
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.assertEquals;
+
 public class RangoTest {
-    /**
-     * Prueba unitaria para el método imprimirNumerosEnRango.
-     */
     @Test
-    public void testImprimirNumerosEnRango() {
-        Rango.imprimirNumerosEnRango(2, 6);
-        // Esperamos que se imprima: 3, 4, 5
+    public void imprimirNumerosEnRango_impresionCorrecta() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
 
-        Rango.imprimirNumerosEnRango(10, 20);
-        // Esperamos que se imprima: 11, 12, 13, 14, 15, 16, 17, 18, 19
+        Rango.imprimirNumerosEnRango(1, 10);
 
-        Rango.imprimirNumerosEnRango(7, 7);
-        // Esperamos que se imprima: No hay números en el rango especificado.
+        System.setOut(System.out);
+
+        String salida = outputStream.toString().trim();
+
+        String resultadoEsperado = "2\n4\n6\n8";
+        assertEquals(resultadoEsperado, salida);
     }
 }
+
